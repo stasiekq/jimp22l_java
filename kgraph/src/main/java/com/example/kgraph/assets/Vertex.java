ada66d47cec18c85332b8717f367702615e86871
stasiekq <staszek5286@gmail.com>
2022-05-30 17:40:52 +0200

    wczytywanie i struktury

---
+++ kgraph/src/main/java/com/example/kgraph/assets/Vertex.java
@@ -0,0 +1,24 @@
+package com.example.kgraph.assets;
+
+import java.util.ArrayList;
+
+public class Vertex {
+    private int id ;
+    private ArrayList <Integer> neighbours = new ArrayList<Integer>();
+    private ArrayList <Double> weights = new ArrayList<Double>();
+    private static int vertNumber = 0;
+
+    public Vertex() {
+        id = ++vertNumber;
+    }
+
+    public void addNeigh(int i) {
+        this.neighbours.add(i);
+    }
+
+    public void addWeight(Double d) {
+        this.weights.add(d);
+    }
+
+
+}
