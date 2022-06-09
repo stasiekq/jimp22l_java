85ba3def50106d35703c14472bd864e733f8f283
MilczarekJan <101673349+MilczarekJan@users.noreply.github.com>
2022-06-09 21:31:53 +0200

    Wiekszość działa

--- kgraph/src/main/java/com/example/kgraph/assets/HelloController.java
+++ kgraph/src/main/java/com/example/kgraph/assets/HelloController.java
@@ -11,6 +11,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class HelloController {
@@ -52,27 +55,33 @@ public class HelloController {
 
     Grid g = new Grid();
 
+    double[] kordyX;
+
+    double[] kordyY;
+    int clickedPoints = 0;
+    int pierwszy = 0;
+    int ostatni = 0;
+
     @FXML
     protected void chooseCoherent() {
         niespojny.setSelected(false);
         spojny.setSelected(true);
     }
 
-    double[] kordyX = new double[100 * 100];
-
-    double[] kordyY = new double[100 * 100];
 
     public void wyswietlGraf(Grid g) {
         int licznik = 0;
+        kordyX = new double[g.columns * g.rows];
+        kordyY = new double[g.columns * g.rows];
         canvas = can.getGraphicsContext2D();
         clearCanvas(canvas, can);
         srednica = 60;
         coordinateY = 0 + srednica / 2;
+        canvas.setStroke(Color.LIGHTBLUE);
 
         while (srednica * g.columns * 2 >= max || srednica * g.rows * 2 >= max) {
             srednica /= 1.5;
         }
-
         for (int i = 0; i < g.rows; i++) {
             coordinateY += srednica * 2;
             coordinateX = 0 + srednica / 2;
@@ -80,12 +89,33 @@ public class HelloController {
             for (int j = 0; j < g.columns; j++) {
                 canvas.setFill(Color.LIGHTBLUE);
                 canvas.fillOval(coordinateX, coordinateY, srednica, srednica);
-                coordinateX += srednica * 2;
                 kordyY[licznik] = coordinateY;
                 kordyX[licznik] = coordinateX;
+                coordinateX += srednica * 2;
                 licznik++;
             }
         }
+        for (int i = 0; i < g.columns * g.rows; i++) {
+
+            if ((i + 1) % g.columns != 0) {
+                canvas.strokeLine(kordyX[i] + srednica, kordyY[i] + srednica/2, kordyX[i] + srednica*2,kordyY[i] + srednica/2);
+            }
+            if (i % g.columns != 0) {
+                canvas.strokeLine(kordyX[i], kordyY[i] + srednica/2, kordyX[i] - srednica, kordyY[i] + srednica/2);
+            }
+            if (i >= g.columns) {
+                canvas.strokeLine(kordyX[i] + srednica/2, kordyY[i], kordyX[i] + srednica/2, kordyY[i] - srednica);
+            }
+            if (i < (g.rows - 1) * g.columns) {
+                canvas.strokeLine(kordyX[i] + srednica/2, kordyY[i] + srednica, kordyX[i] + srednica/2, kordyY[i] + srednica*2);
+            }
+        }
+        for (int i = 0; i < licznik; i++)
+        {
+            System.out.println(kordyX[i]);
+            System.out.println(kordyY[i]);
+        }
+        System.out.println("Srednica przy generowaniu: " + srednica);
     }
 
     public void clearCanvas(GraphicsContext gc, Canvas cv) {
@@ -199,7 +229,7 @@ public class HelloController {
         //*/
     }
 
-    int clickedPoints = 0;
+
 
     /*
     @FXML
@@ -234,19 +264,53 @@ public class HelloController {
      */
     public void uruchomDijsktra(int pierwszy, int ostatni) {
         Sciezka doWykresu = Dijkstra.Algorytm(AdjacMat.Macierz(g), pierwszy, ostatni);
-        for (int i = 0; i < doWykresu.vertices.size(); i++) {
-            System.out.println("Wezel " + i + " " + doWykresu.vertices.get(i));
-        }
         canvas.setStroke(Color.INDIANRED);
+        canvas.setFill(Color.INDIANRED);
+        for (int i = 0; i < doWykresu.vertices.size(); i++) {
+            System.out.println("Wezel " + doWykresu.vertices.get(i) + " X: " + kordyX[doWykresu.vertices.get(i)] + " Y: " + kordyY[doWykresu.vertices.get(i)]);
+        }
+        for (int i = 0; i < doWykresu.vertices.size(); i++) {
+            canvas.fillOval(kordyX[doWykresu.vertices.get(i)], kordyY[doWykresu.vertices.get(i)], srednica, srednica);
+        }
+        for (int i = 0; i < doWykresu.vertices.size(); i++) {
+            canvas.fillOval(kordyX[doWykresu.vertices.get(i)], kordyY[doWykresu.vertices.get(i)], srednica, srednica);
+        }
+        for (int i = 0; i < doWykresu.vertices.size() - 1; i++) {
+            if (doWykresu.vertices.get(i + 1) == doWykresu.vertices.get(i) - 1)
+            {
+                System.out.println("Lewo");
+                canvas.strokeLine(kordyX[doWykresu.vertices.get(i)], kordyY[doWykresu.vertices.get(i)] + srednica/2, kordyX[doWykresu.vertices.get(i)] - srednica, kordyY[doWykresu.vertices.get(i)] + srednica/2);
+            }
+            if(doWykresu.vertices.get(i + 1) == doWykresu.vertices.get(i) + 1)
+            {
+                System.out.println("Prawo");
+                canvas.strokeLine(kordyX[doWykresu.vertices.get(i)] + srednica, kordyY[doWykresu.vertices.get(i)] + srednica/2, kordyX[doWykresu.vertices.get(i)] + srednica*2,kordyY[doWykresu.vertices.get(i)] + srednica/2);
+            }
+            if(doWykresu.vertices.get(i + 1) == doWykresu.vertices.get(i) - g.columns)
+            {
+                System.out.println("Góra");
+                canvas.strokeLine(kordyX[doWykresu.vertices.get(i)] + srednica/2, kordyY[doWykresu.vertices.get(i)], kordyX[doWykresu.vertices.get(i)] + srednica/2, kordyY[doWykresu.vertices.get(i)] - srednica);
+            }
+            if(doWykresu.vertices.get(i + 1) == doWykresu.vertices.get(i) + g.columns)
+            {
+                System.out.println("Dół");
+                canvas.strokeLine(kordyX[doWykresu.vertices.get(i)] + srednica/2, kordyY[doWykresu.vertices.get(i)] + srednica, kordyX[doWykresu.vertices.get(i)] + srednica/2, kordyY[doWykresu.vertices.get(i)] + srednica*2);
+            }
+        }
+        txt_field6.setText("Dlugosc trasy: " + String.format("%.4f", doWykresu.waga));
+        txt_field7.setText(String.valueOf(pierwszy));
+        txt_field8.setText(String.valueOf(ostatni));
+
+        /*
         int licznik = 0;
         int licznikSciezka = 0;
         for (int j = 0; j < g.rows; j++) {
             for (int i = 0; i < g.columns; i++) {
                 if (licznik == doWykresu.vertices.get(licznikSciezka) && licznikSciezka + 1 < doWykresu.vertices.size()) {
-                    System.out.println(licznik + " ," + licznikSciezka);
-                    System.out.println("Linia X: " + kordyX[doWykresu.vertices.get(licznikSciezka)]);
-                    System.out.println("Linia Y: " + kordyY[doWykresu.vertices.get(licznikSciezka)]);
-                    System.out.println(srednica);
+                    //System.out.println(licznik + " ," + licznikSciezka);
+                    //System.out.println("Linia X: " + kordyX[doWykresu.vertices.get(licznikSciezka)]);
+                    //System.out.println("Linia Y: " + kordyY[doWykresu.vertices.get(licznikSciezka)]);
+                    //System.out.println(srednica);
                     if (doWykresu.vertices.get(licznikSciezka) == doWykresu.vertices.get(licznikSciezka + 1) + 1) {
                         canvas.strokeLine(kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica, kordyY[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica * 2, kordyY[doWykresu.vertices.get(licznikSciezka)] + srednica / 2);
                     } else if (doWykresu.vertices.get(licznikSciezka) == doWykresu.vertices.get(licznikSciezka + 1) - 1) {
@@ -254,13 +318,14 @@ public class HelloController {
                     } else if (doWykresu.vertices.get(licznikSciezka) == doWykresu.vertices.get(licznikSciezka + 1) - g.columns) {
                         canvas.strokeLine(kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyY[doWykresu.vertices.get(licznikSciezka)], kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyY[doWykresu.vertices.get(licznikSciezka)] + srednica);
                     } else if (doWykresu.vertices.get(licznikSciezka) == doWykresu.vertices.get(licznikSciezka + 1) + g.columns) {
-                        canvas.strokeLine(kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyY[doWykresu.vertices.get(licznikSciezka)], kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyY[doWykresu.vertices.get(licznikSciezka)] - srednica * 2);
+                        canvas.strokeLine(kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyY[doWykresu.vertices.get(licznikSciezka)] - srednica, kordyX[doWykresu.vertices.get(licznikSciezka)] + srednica / 2, kordyY[doWykresu.vertices.get(licznikSciezka)] - srednica * 2);
                     }
                     licznikSciezka++;
                 }
                 licznik++;
             }
         }
+        */
     }
 
     public void rozpoczecieRys(MouseEvent mouseEvent) throws IOException {
@@ -273,31 +338,64 @@ public class HelloController {
         coordinateY = 0 + srednica / 2;
 
         int licznik = 0;
-        int pierwszy = 0;
-        int ostatni = 0;
-
+        System.out.println("Myszka X: " + mouseX);
+        System.out.println("Myszka Y: " + mouseX);
+        for (int i = 0; i < g.rows * g.columns; i++){
+            if ((mouseX >= kordyX[i] && mouseX <= kordyX[i] + srednica) && (mouseY >= kordyY[i] && mouseY <= kordyY[i] + srednica * 2))
+            {
+                if (clickedPoints % 2 == 1) {
+                    if (clickedPoints != 1)
+                    {
+                        wyswietlGraf(g);
+                    }
+                    pierwszy = i;
+                    canvas.setStroke(Color.BLACK);
+                    canvas.strokeOval(kordyX[i], kordyY[i], srednica, srednica);
+                } else if (clickedPoints % 2 == 0) {
+                    ostatni = i;
+                    canvas.setStroke(Color.BLACK);
+                    canvas.strokeOval(kordyX[i], kordyY[i], srednica, srednica);
+                    uruchomDijsktra(pierwszy, ostatni);
+                }
+            }
+        }
+        /*
         for (int i = 0; i < g.rows; i++) {
             coordinateY += srednica * 2;
             coordinateX = 0 + srednica / 2;
             for (int j = 0; j < g.columns; j++) {
+                if ((mouseX >= kordyX[i * g.rows + j] && mouseX <= kordyX[i * g.rows + j] + srednica) && (mouseY >= kordyY[i * g.rows + j] && mouseY <= kordyY[i * g.rows + j] + srednica * 2)) {
+                    if (clickedPoints % 2 == 1) {
+                        pierwszy = licznik;
+                        canvas.setStroke(Color.BLACK);
+                        canvas.strokeOval(kordyX[i * g.rows + j], kordyY[i * g.rows + j] + srednica / (2), srednica, srednica);
+                    } else if (clickedPoints % 2 == 0) {
+                        ostatni = licznik;
+                        canvas.setStroke(Color.BLACK);
+                        canvas.strokeOval(kordyX[i * g.rows + j], kordyY[i * g.rows + j] + srednica / (2), srednica, srednica);
+                        uruchomDijsktra(pierwszy, ostatni);
+                    }
+                }
+            }
                 if ((mouseX >= coordinateX && mouseX <= coordinateX + srednica) && (mouseY >= coordinateY && mouseY <= coordinateY + srednica * 2)) {
                     if (clickedPoints % 2 == 1) {
                         //numPath++;
                         //clearPath(); // TRZEBA DODAĆ USUWANIE ŚCIEŻEK
                         pierwszy = licznik;
                         canvas.setStroke(Color.BLACK);
-                        canvas.strokeOval(coordinateX, coordinateY + srednica / (1.5), srednica, srednica);
+                        canvas.strokeOval(kordyX[i*g.rows + j], kordyY[i*g.rows + j] + srednica / (2), srednica, srednica);
                     } else if (clickedPoints % 2 == 0) {
                         ostatni = licznik;
                         canvas.setStroke(Color.BLACK);
-                        canvas.strokeOval(coordinateX, coordinateY + srednica / (1.5), srednica, srednica);
+                        canvas.strokeOval(kordyX[i*g.rows + j], kordyY[i*g.rows + j] + srednica / (2), srednica, srednica);
                         uruchomDijsktra(pierwszy, ostatni);
                     }
                 }
                 coordinateX += srednica * 2;
                 licznik++;
+
             }
+
+         */
         }
     }
-
-}
